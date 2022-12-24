719fa4116766a2355c3b55f4ef3324dcffff114f
JoaoCupido <jcupido13@gmail.com>
2022-12-24 16:18:10 +0000

    added isExit so that Exit Button can actually Quit the Project on Build

--- Assets/Scripts/changeScene.cs
+++ Assets/Scripts/changeScene.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class changeScene : MonoBehaviour
 {
     [SerializeField]
     string goToScene;
+    [SerializeField]
+    bool isExit = false;
 
     void Start()
     {
+        if(isExit)
+        {
+            Application.Quit();
+        }
         Button btn = gameObject.GetComponent<Button>();
         btn.onClick.AddListener(ModifyScene);
     }
